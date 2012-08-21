501505068f8303494a577c1d07a18164168e5245
Tomaž Šuštar <tomaz@zelnik.net>
2012-08-21 19:56:48 +0200

    Popravljena dovoljenja za komande na kontrolerjih

--- protected/controllers/StraniController.php
+++ protected/controllers/StraniController.php
@@ -36,7 +36,7 @@ class StraniController extends Controller
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete', 'uvozi'),
-				'users'=>array('admin'),
+				'users'=>array('@'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
