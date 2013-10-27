26b86725f1e2a30c14f3f17bcf007235a18252f9
archer <frojnd@localhost.localdomain>
2013-10-27 16:24:24 +0100

    dodan ICalParser

---
+++ protected/components/parsers/ICalParser.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once("../../vendors/ics-parser-read-only/ICal.php");
+
+class ICalParser extends ICal {
+
+    public function izpisiNekaj() {
+        echo "nekaj izpisano" . "<br />";
+    }
+
+}
+
+
+?>
