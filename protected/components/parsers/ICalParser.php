1a1a9f26c79ef6cf5f78df11b5d5be2ca01a7914
archer <frojnd@localhost.localdomain>
2013-11-04 14:53:43 +0100

    updated icalparser

--- protected/components/parsers/ICalParser.php
+++ protected/components/parsers/ICalParser.php
@@ -1,8 +1,14 @@
 <?php
 
-require_once("../../vendors/ics-parser-read-only/ICal.php");
+require_once Yii::app()->basePath.'/vendors/ics-parser-read-only/ICal.php.back.php';
+require_once Yii::app()->basePath.'/vendors/ics-parser-read-only/MyCal.ics';
 
 class ICalParser extends ICal {
+    public $filename = 'MyCal.ics';
+
+    public function __construct() {
+        parent::__construct($this->filename);
+    }
 
     public function izpisiNekaj() {
         echo "nekaj izpisano" . "<br />";
